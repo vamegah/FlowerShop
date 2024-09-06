022247008e31ea33fd67e8c8c6d4d6e65d9b914c
RicArora1991 <richaaeri9988@gmail.com>
2024-09-06 17:57:34 +0530

    css change

--- FlowerLandingPage.js
+++ FlowerLandingPage.js
@@ -14,75 +14,80 @@ export default function FlowerlandingPage() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: '#fff',
-  },
-  header: {
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  headerText: {
-    fontSize: 28,
-    fontWeight: 'bold',
-  },
-  subHeaderText: {
-    fontSize: 16,
-    color: '#555',
-  },
-  imageContainer: {
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  image: {
-    width: '100%',
-    height: 150,
-    borderRadius: 5,
-  },
-  searchContainer: {
-    marginBottom: 10,
-  },
-  textInput: {
-    height: 40,
-    borderColor: '#ddd',
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingHorizontal: 10,
-  },
-  featuredSection: {
-    marginBottom: 10,
-  },
-  sectionTitle: {
-    fontSize: 22,
-    fontWeight: 'bold',
-    marginBottom: 5,
-  },
-  flowerCard: {
-    marginRight: 10,
-    alignItems: 'center',
-  },
-  flowerImage: {
-    width: 120,
-    height: 80,
-    borderRadius: 5,
-  },
-  flowerTitle: {
-    marginTop: 5,
-    fontSize: 14,
-    fontWeight: 'bold',
-  },
-  buttonContainer: {
-    alignItems: 'center',
-  },
-  button: {
-    backgroundColor: '#f56c6c',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-});
+    container: {
+      flex: 1,
+      padding: 10,
+      backgroundColor: '#fff',
+    },
+    header: {
+      alignItems: 'center',
+      marginBottom: 10,
+    },
+    headerText: {
+      fontSize: 38,
+      fontWeight: 'bold',
+    },
+    subHeaderText: {
+      fontSize: 27,
+      color: '#555',
+      fontStyle:'italic',
+      color:'#f56c6c'
+    },
+    imageContainer: {
+      alignItems: 'center',
+      marginBottom: 10,
+    },
+    image: {
+      width: '100%',
+      height: 250,
+      borderRadius: 5,
+    },
+    searchContainer: {
+      marginBottom: 10,
+    },
+    textInput: {
+      height: 90,
+      borderColor: '#ddd',
+      borderWidth: 1,
+      borderRadius: 5,
+      paddingHorizontal: 10,
+      fontSize:25
+    },
+    featuredSection: {
+      marginBottom: 10,
+  
+    },
+    sectionTitle: {
+      fontSize: 28,
+      fontWeight: 'bold',
+      marginBottom: 5,
+    },
+    flowerCard: {
+      marginRight: 10,
+      alignItems: 'center',
+    },
+    flowerImage: {
+      width: 270,
+      height: 210,
+      borderRadius: 5,
+    },
+    flowerTitle: {
+      marginTop: 5,
+      fontSize: 14,
+      fontWeight: 'bold',
+    },
+    buttonContainer: {
+      alignItems: 'center',
+    },
+    button: {
+      backgroundColor: '#f56c6c',
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      borderRadius: 5,
+    },
+    buttonText: {
+      color: '#fff',
+      fontWeight: 'bold',
+      fontSize:'30px'
+    },
+  });
